894eac046ed67b17ede45b563a03c7d41e58b8a9
Lukasz <michalski469@gmail.com>
2025-08-07 22:48:34 +0200

    test, cleanup

---
+++ PhishApp/PhishApp.WebApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhishApp.WebApi.Helpers;
+
+namespace PhishApp.WebApi.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route(Routes.Ping)]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
---
+++ PhishApp/PhishApp.WebApi/Helpers/Routes.cs
@@ -0,0 +1,8 @@
+﻿namespace PhishApp.WebApi.Helpers
+{
+    public class Routes
+    {
+        public const string Ping = "api/ping";
+        public const string PingAuthorized = "api/ping/authorized";
+    }
+}
